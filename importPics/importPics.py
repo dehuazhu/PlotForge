1c5231bb9d1f33cd0d8663ed00f64f255e9b0601
martinabuck <martinabuck@live.com>
2019-11-30 15:23:42 +0100

    add Comments defining structures
    add random function to test jupyter notebook

--- importPics/importPics.py
+++ importPics/importPics.py
@@ -1,4 +1,17 @@
-##############Import Pictures#######################
-#Author: Martina Buck
+#####################################
+# Module importPics
+# Description: TODO
+# Author: Martina Buck
+##################################
 
-#import .tif
+
+# import packages
+
+
+# classes and functions
+
+# import .tif
+def addNumbers(a, b):
+    return a+b
+
+# main function for testing
